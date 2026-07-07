0cdec9e68a1a19d3ab22e0a418a4740c15878e36
Lunaris47 <sciamannajesse@gmail.com>
2026-07-07 14:47:13 -0700

    Initial Spring Boot project setup with PostgreSQL and Neon connection

---
+++ src/main/java/com/flara/FlaraApiApplication.java
@@ -0,0 +1,13 @@
+package com.flara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flara/FlaraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlaraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
